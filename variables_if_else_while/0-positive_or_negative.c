38fc70d9ab52ff457d6239059586d6a6bd0364c3
SacariLuz <asacaricachi@gmail.com>
2023-08-02 22:16:16 -0700

    This program will assign a random number

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints if number is negative, positive or zero
+ * main - Prints if number is positive, zero or negative
  *
- * Return: Always - Success
+ * Return: Always (Success)
  */
 int main(void)
 {
@@ -24,6 +26,6 @@ int main(void)
     {
         printf("%d is negative\n", n);
     }
-    
+
     return (0);
 }
