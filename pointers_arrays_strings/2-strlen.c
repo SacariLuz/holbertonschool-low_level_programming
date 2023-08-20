478d785b0e22e8987d5d8806138d72bfde66ed82
SacariLuz <asacaricachi@gmail.com>
2023-08-19 20:58:25 -0700

    length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - Print in  length of a string
+ * @s: Value
+ *
+ *Return: len of a string
+ */
+int _strlen(char *s)
+{
+	size_t len = strlen(s);
+
+	return (len);
+}
