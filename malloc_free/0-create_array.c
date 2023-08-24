9d5649639a1523c18ce3ce6c5d491d6e46f6f616
SacariLuz <asacaricachi@gmail.com>
2023-08-23 21:01:59 -0700

    create an array of chars

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char* ptr;
+	unsigned int conter;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * sizeof(char));
+	{
+	if (ptr == NULL)
+		return (NULL);
+	}
+	conter = 0;
+	while (conter < size)
+	{
+		ptr[conter] = c;
+		conter++;
+	}
+	return (ptr);
+}
